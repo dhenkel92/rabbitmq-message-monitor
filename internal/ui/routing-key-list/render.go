2a9e7cf3b66b496884bffc120b657f18b77b6925
Daniel Henkel <Daniel.henkel@westwing.de>
2021-11-29 07:59:42 +0100

    add sorting arrows to routing key header

--- internal/ui/routing-key-list/render.go
+++ internal/ui/routing-key-list/render.go
@@ -11,12 +11,36 @@ func (list *RoutingKeyList) renderList() {
 
 	rtL, countL, sizeL := calculateCellWidths(list.list.Inner.Max.X)
 
-	result = append(result, fmt.Sprintf("[%-*s](fg:white) [%-*s](fg:white) [%-*s](fg:white)", rtL, "Routing Key", countL, "Count", sizeL, "Total Size"))
+	result = append(result, list.renderHeader())
 	for _, entry := range list.data {
 		result = append(result, fmt.Sprintf("%-*s %-*d %-*s", rtL, entry.RoutingKey, countL, entry.Count, sizeL, helper.FormatBytesToMb(entry.TotalBytes)))
 	}
 
 	list.list.Rows = result
+}
+
+func (list *RoutingKeyList) renderHeader() string {
+	rtL, countL, sizeL := calculateCellWidths(list.list.Inner.Max.X)
+
+	rkH := "Routing Key"
+	countH := "Count"
+	sizeH := "Total Size"
+	switch list.sorting {
+	case RK_SORTING_NAME_ASC:
+		rkH += " ▲"
+	case RK_SORTING_NAME_DESC:
+		rkH += " ▼"
+	case RK_SORTING_COUNT_ASC:
+		countH += " ▲"
+	case RK_SORTING_COUNT_DESC:
+		countH += " ▼"
+	case RK_SORTING_TOTAL_SIZE_ASC:
+		sizeH += " ▲"
+	case RK_SORTING_TOTAL_SIZE_DESC:
+		sizeH += " ▼"
+	}
+
+	return fmt.Sprintf("[%-*s](fg:white) [%-*s](fg:white) [%-*s](fg:white)", rtL, rkH, countL, countH, sizeL, sizeH)
 }
 
 func calculateCellWidths(rowLenght int) (int, int, int) {
